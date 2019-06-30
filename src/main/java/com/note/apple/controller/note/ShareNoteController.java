e148fb4903733c3763a3591cf1de311d7a971aba
liuke <liuke901112@163.com>
2019-06-30 17:29:01 +0800

    去掉不需要的引用

--- src/main/java/com/note/apple/controller/note/ShareNoteController.java
+++ src/main/java/com/note/apple/controller/note/ShareNoteController.java
@@ -1,13 +1,13 @@
 package com.note.apple.controller.note;
 
-import javax.annotation.Resource;
 import com.note.apple.data.NoteResult;
 import com.note.apple.service.NoteService;
-import com.sun.xml.internal.bind.v2.TODO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 @CrossOrigin
 @Controller
